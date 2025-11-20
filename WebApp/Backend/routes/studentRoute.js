8077fed81a9711a62acab56752001824b93f70d9
sandeshprasai <sandeshprasai4321@gmail.com>
2025-11-20 16:05:12 +0545

    Update: Removed Unused Routes and Controller

--- WebApp/Backend/routes/studentRoute.js
+++
@@ -1,8 +0,0 @@
-const express= require("express");
-const router= express.Router();
-const {addStudent, getStudentData}= require("../controllers/studentController");
-
-router.post("/add-student", addStudent);
-router.get("/get-students", getStudentData);
-
-module.exports= router;
