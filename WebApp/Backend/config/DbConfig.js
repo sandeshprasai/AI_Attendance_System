e62f26b51f4b02fd3f2b6d5bbc41fd965b418385
sandeshprasai <sandeshprasai4321@gmail.com>
2025-11-20 16:00:56 +0545

    Feat:Created an admin Account

---
+++ WebApp/Backend/config/DbConfig.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require("dotenv").config({path:"../../.env"});
+
+async function DataBaseConnection() {
+  try {
+    await  mongoose.connect(process.env.DATABASE_URI);
+    console.log("Database Connected Successfully");
+  } catch (error) {
+    console.error(`Error connecting to MongoDb database ${error}`);
+  }
+}
+
+
+module.exports = DataBaseConnection
---
+++ WebApp/Backend/models/users.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const users = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["admin", "teacher", "student"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("users", users);
