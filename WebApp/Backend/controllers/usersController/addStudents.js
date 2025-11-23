061dbd56950b7d0cd0045008a80ffa85fe092028
sandeshprasai <sandeshprasai4321@gmail.com>
2025-11-23 20:59:19 +0545

    Update(students): add student registration with email notification - Implement student account registration after adding student records - Set up email service to send credentials (username/password) to students

--- WebApp/Backend/controllers/usersController/addStudents.js
+++ WebApp/Backend/controllers/usersController/addStudents.js
@@ -1,29 +1,84 @@
 const students = require("./../../models/students");
+const generateCredentials = require("./../../middlewares/generateCredentials");
+const sendCredentialsEmail = require("./../../middlewares/mailCredentials");
+const bcryptjs = require("bcryptjs");
+const users = require("./../../models/users");
+const mongoose = require("mongoose");
 
 const addStudents = async (req, res) => {
-  console.log(req.body);
-  try {
-    const newStudent = new students({
-      ...req.body,
-    });
+  const session = await mongoose.startSession();
+  session.startTransaction();
 
-    const existingStudent = await students.findOne({
-      RollNo: req.body.RollNo,
-    });
+  try {
+    const existingStudent = await students.findOne(
+      { RollNo: req.body.RollNo },
+      null,
+      { session }
+    );
 
     if (existingStudent) {
-      return res
-        .status(400)
-        .json({ error: "Student with provided Roll No already registered " });
+      await session.abortTransaction();
+      session.endSession();
+      return res.status(400).json({
+        error: "Student with provided Roll No already registered",
+      });
     }
-    await newStudent.save();
-    return res
-      .status(200)
-      .json({ success: true, message: "Student added Successfully" });
+
+    const newStudent = new students({ ...req.body });
+    let { username, password } = generateCredentials(
+      newStudent.FullName,
+      newStudent.RollNo
+    );
+    const userExists = await users.findOne({ email: username }, null, {
+      session,
+    });
+    if (userExists) {
+      username = `${username}_${Math.floor(Math.random() * 1000)}`;
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    await newStudent.save({ session });
+    await users.create(
+      [
+        {
+          email: username,
+          password: hashedPassword,
+          name: newStudent.FullName,
+          role: "student",
+        },
+      ],
+      { session }
+    );
+    const emailSent = await sendCredentialsEmail(
+      newStudent.Email,
+      username,
+      password
+    );
+
+    if (!emailSent) {
+      throw new Error("Failed to send credentials email");
+    }
+    await session.commitTransaction();
+    session.endSession();
+
+    return res.status(200).json({
+      success: true,
+      message: "Student registered and user account created",
+    });
   } catch (err) {
-    console.error(`An error occured while adding Student. ${err}`);
+    await session.abortTransaction();
+    session.endSession();
+    console.error(`Error while adding student: ${err}`);
+    if (err.code === 11000) {
+      const duplicateField = Object.keys(err.keyPattern)[0];
+      return res.status(400).json({
+        error: `Duplicate value for field '${duplicateField}'.`,
+      });
+    }
     return res.status(500).json({
-      error: `An error occured while adding Student. Internal server error`,
+      error:
+        "An error occurred while adding student. All changes were rolled back.",
     });
   }
 };
