60069fc16848c7c698a87b6d4759f4b5e43c8c10
santu yadav <115726207+yadavsantu@users.noreply.github.com>
2025-12-02 15:24:51 +0545

    feat: add loading state to AddStudent form and prevent double submission

--- WebApp/frontend/src/components/ui/InputGroup.jsx
+++ WebApp/frontend/src/components/ui/InputGroup.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const InputGroup = ({ label, name, type, value, onChange, placeholder, Icon, error }) => {
+const InputGroup = ({ label, name, type, value, onChange, placeholder, Icon, error,disabled, }) => {
   return (
     <div className="space-y-1">
       <label className="text-sm font-bold flex items-center gap-2">
@@ -13,10 +13,13 @@ const InputGroup = ({ label, name, type, value, onChange, placeholder, Icon, err
         name={name}
         value={value}
         onChange={onChange}
+        disabled={disabled} 
         placeholder={placeholder}
         className={`w-full h-12 px-4 border-2 rounded-xl focus:outline-none ${
           error ? "border-red-500 focus:border-red-500" : "border-emerald-400 focus:border-emerald-600"
-        }`}
+        }`
+        
+      }
       />
 
       {error && <p className="text-red-500 text-sm">{error}</p>}
