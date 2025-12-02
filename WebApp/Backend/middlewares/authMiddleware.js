d01696d9bfe8ff6459e0a220c0c6c6a969f40923
sandeshprasai <sandeshprasai4321@gmail.com>
2025-12-02 23:02:50 +0545

    Feat(Middleware): Added a middleware to check access token and verify the user role before adding the students

--- WebApp/Backend/middlewares/authMiddleware.js
+++ WebApp/Backend/middlewares/authMiddleware.js
@@ -2,12 +2,12 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; // Bearer <token>
+  const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "No token provided" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // contains id, email, role
+    req.user = decoded;
     next();
   } catch (err) {
     return res.status(401).json({ message: "Invalid token" });
--- WebApp/Backend/routes/usersRoutes.js
+++ WebApp/Backend/routes/usersRoutes.js
@@ -3,11 +3,12 @@ const userRouter = express.Router();
 const sanitizeStudenstInput = require("./../middlewares/sanitizeStudentsInput");
 const addStudent = require("./../controllers/usersController/addStudents");
 const uploadFile = require("./../middlewares/profileImageHelper");
+const preventAccess  = require( "./../middlewares/adminAccessControl");
 
 userRouter.get("/", (req, res) => {
   res.send("You are on user routes");
 });
 
-userRouter.post("/students", uploadFile, sanitizeStudenstInput, addStudent);
+userRouter.post("/students",preventAccess, uploadFile, sanitizeStudenstInput, addStudent);
 
 module.exports = userRouter;
