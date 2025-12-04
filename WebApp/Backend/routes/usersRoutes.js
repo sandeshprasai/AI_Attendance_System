52cc0532bfeeebb67cac7422a600fd39bb4fa6ed
santu yadav <115726207+yadavsantu@users.noreply.github.com>
2025-12-05 01:57:47 +0545

    feat(users-backend): add stats and all users endpoints with pagination and filtering

--- WebApp/Backend/routes/usersRoutes.js
+++ WebApp/Backend/routes/usersRoutes.js
@@ -4,11 +4,20 @@ const sanitizeStudenstInput = require("./../middlewares/sanitizeStudentsInput");
 const addStudent = require("./../controllers/usersController/addStudents");
 const uploadFile = require("./../middlewares/profileImageHelper");
 const preventAccess  = require( "./../middlewares/adminAccessControl");
+const getUserStats = require("./../controllers/usersController/getUserStats");
+const getAllUsers = require("./../controllers/usersController/getAllUsers");
+
+// Test Route
 
 userRouter.get("/", (req, res) => {
   res.send("You are on user routes");
 });
 
 userRouter.post("/students",preventAccess, uploadFile, sanitizeStudenstInput, addStudent);
+// NEW backend integration routes
+userRouter.get("/stats", preventAccess, getUserStats);
+userRouter.get("/all", preventAccess, getAllUsers);
+
+
 
 module.exports = userRouter;
